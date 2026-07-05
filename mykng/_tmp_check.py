d9c37e6c3e4b6db70f53532992ae049953781fa1
liangzi <huliang2019@163.com>
2026-07-05 22:29:41 +0800

    chore: clean up mykng root - remove deprecated deploy engine, Jenkinsfile, temp scripts

--- mykng/_tmp_check.py
+++
@@ -1,9 +0,0 @@
-import paramiko
-
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('120.26.66.182', port=3385, username='root', password='root', timeout=15)
-stdin, stdout, stderr = ssh.exec_command('cd /root/devtools/mykng && docker ps --format "table {{.Names}}\t{{.Status}}"')
-print(stdout.read().decode())
-print(stderr.read().decode())
-ssh.close()
--- mykng/kb-web.bak_/src/config.ts
+++
@@ -1,2 +0,0 @@
-/** 统一上下文路径（从 .env VITE_CONTEXT_PATH 读取，唯一派生点） */
-export const CONTEXT_PATH = import.meta.env.VITE_CONTEXT_PATH || '/kb'
